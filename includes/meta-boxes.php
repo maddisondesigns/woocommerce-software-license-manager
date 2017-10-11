7b38cfed3560f7cbf05b56718d4e9b3eaa824f94
Anthony Hortin <anthony@maddisondesigns.com>
2017-10-12 05:19:06 +1100

    Add form fields inside Option Group and assign class so they'll only display for downloadable products. Removed description text and add as Help Tips. Improve data escaping and checks prior to saving to database.

--- includes/meta-boxes.php
+++ includes/meta-boxes.php
@@ -22,34 +22,37 @@ function wc_slm_custom_general_fields() {
 		$_wc_slm_licensing_renewal_period = 0;
 	}
 	?>
-	<script type="text/javascript">
-	jQuery( document ).ready( function($) {
-		$( "#_wc_slm_licensing_enabled" ).on( "click",function() {
-			// TODO: Improve toggle handling and prevent double display
-			$( ".wc-slm-variable-toggled-hide" ).toggle();
-			$( ".wc-slm-toggled-hide" ).toggle();
+	<!-- Only show the License Key form fields if the product is downloadable -->
+	<div class="options_group show_if_downloadable">
+		<script type="text/javascript">
+		jQuery( document ).ready( function($) {
+			$( "#_wc_slm_licensing_enabled" ).on( "click",function() {
+				// TODO: Improve toggle handling and prevent double display
+				$( ".wc-slm-variable-toggled-hide" ).toggle();
+				$( ".wc-slm-toggled-hide" ).toggle();
+			} );
 		} );
-	} );
-	</script>
+		</script>
 
-	<p class="form-field">
-		<input type="checkbox" name="_wc_slm_licensing_enabled" id="_wc_slm_licensing_enabled" value="1" <?php echo checked(true, $wc_slm_licensing_enabled, false); ?> />
-		<label for="_wc_slm_licensing_enabled"><?php _e('Enable Software Licensing', 'wc-slm');?></label>
-	</p>
+		<p class="form-field">
+			<input type="checkbox" name="_wc_slm_licensing_enabled" id="_wc_slm_licensing_enabled" value="1" <?php echo checked(true, $wc_slm_licensing_enabled, false); ?> />
+			<label for="_wc_slm_licensing_enabled"><?php _e('Enable Software Licensing', 'wc-slm');?></label>
+		</p>
 
-	<div <?php echo $wc_slm_display; ?> class="wc-slm-toggled-hide">
-		<p class="form-field">
-			<label for="_wc_slm_licensing_renewal_period">
-				<?php _e( 'Renewal period (Yearly)', 'wc-slm' );?>
-			</label>
-			<input type="number" name="_wc_slm_licensing_renewal_period" id="_wc_slm_licensing_renewal_period" value="<?php echo $_wc_slm_licensing_renewal_period; ?>"  />
-			<span class="description"><?php _e( 'Enter 0 (zero) for lifetime renewals.', 'wc-slm' );?></span>
-		</p>
-		<p class="form-field">
-			<label for="_wc_slm_sites_allowed"><?php _e('Number of Sites Allowed', 'wc-slm');?></label>
-			<input type="number" name="_wc_slm_sites_allowed" class="small-text" value="<?php echo $wc_slm_sites_allowed; ?>" />
-			<span class="description"><?php _e( 'Enter the number of sites that can be activated for a single License Key.', 'wc-slm' );?></span>
-		</p>
+		<div <?php echo $wc_slm_display; ?> class="wc-slm-toggled-hide">
+			<p class="form-field">
+				<label for="_wc_slm_licensing_renewal_period">
+					<?php _e( 'Renewal period (Yearly)', 'wc-slm' );?>
+				</label>
+				<input type="number" name="_wc_slm_licensing_renewal_period" id="_wc_slm_licensing_renewal_period" value="<?php echo $_wc_slm_licensing_renewal_period; ?>"  />
+				<span class="woocommerce-help-tip" data-tip="<?php _e( 'Enter the numbr of years for the Renewal Period. Enter 0 (zero) or leave blank for lifetime renewals.', 'wc-slm' );?>"></span>
+			</p>
+			<p class="form-field">
+				<label for="_wc_slm_sites_allowed"><?php _e('Number of Sites Allowed', 'wc-slm');?></label>
+				<input type="number" name="_wc_slm_sites_allowed" class="small-text" value="<?php echo $wc_slm_sites_allowed; ?>" />
+				<span class="woocommerce-help-tip" data-tip="<?php _e( 'Enter the number of sites that can be activated for a single License Key. Value must be greater than 0 (zero)', 'wc-slm' );?>"></span>
+			</p>
+		</div>
 	</div>
 	<?php
 }
@@ -63,17 +66,25 @@ function wc_slm_custom_general_fields_save( $post_id ) {
 	$woocommerce_wc_slm_sites_allowed = $_POST['_wc_slm_sites_allowed'];
 	$_wc_slm_licensing_renewal_period = $_POST['_wc_slm_licensing_renewal_period'];
 
-	if ( !empty( $woocommerce_wc_slm_licensing_enabled ) ) {
-		update_post_meta( $post_id, '_wc_slm_licensing_enabled', esc_html( $woocommerce_wc_slm_licensing_enabled ) );
+	update_post_meta(
+		$post_id,
+		'_wc_slm_licensing_enabled',
+		( isset( $woocommerce_wc_slm_licensing_enabled ) ? 1 : 0 )
+	);
+
+	if ( isset( $woocommerce_wc_slm_sites_allowed ) ) {
+		update_post_meta(
+			$post_id,
+			'_wc_slm_sites_allowed',
+			( absint( $woocommerce_wc_slm_sites_allowed ) >= 1 ? absint( $woocommerce_wc_slm_sites_allowed ) : 1 )
+		);
 	}
 
-	if ( !empty( $woocommerce_wc_slm_sites_allowed ) ) {
-		update_post_meta( $post_id, '_wc_slm_sites_allowed', esc_html( $woocommerce_wc_slm_sites_allowed ) );
-	}
-
-	if ( !empty( $_wc_slm_licensing_renewal_period ) ) {
-		update_post_meta( $post_id, '_wc_slm_licensing_renewal_period', esc_html( $_wc_slm_licensing_renewal_period ) );
-	}
+	update_post_meta(
+		$post_id,
+		'_wc_slm_licensing_renewal_period',
+		( empty( $_wc_slm_licensing_renewal_period ) || absint( $_wc_slm_licensing_renewal_period ) == 0 ? 0 : absint( $_wc_slm_licensing_renewal_period ) )
+	);
 
 }
 add_action( 'woocommerce_process_product_meta', 'wc_slm_custom_general_fields_save' );
